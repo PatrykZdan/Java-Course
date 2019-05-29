b28cfd6b27b21d43e8dfe1fd024ce6d11edb3728
Patryk Zdanowicz <patrykzdan@gmail.com>
2019-05-29 18:41:36 +0200

    Done 5 question from Problem-Set 4.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   Problem-set 4/Q4/.idea/workspace.xml
    	modified:   Problem-set 4/Q4/out/production/Q4/com/company/Main.class
    	modified:   Problem-set 4/Q4/src/com/company/Main.java
    	new file:   Problem-set 4/Q5/.idea/description.html
    	new file:   Problem-set 4/Q5/.idea/encodings.xml
    	new file:   Problem-set 4/Q5/.idea/misc.xml
    	new file:   Problem-set 4/Q5/.idea/modules.xml
    	new file:   Problem-set 4/Q5/.idea/project-template.xml
    	new file:   Problem-set 4/Q5/.idea/vcs.xml
    	new file:   Problem-set 4/Q5/.idea/workspace.xml
    	new file:   Problem-set 4/Q5/Q5.iml
    	new file:   Problem-set 4/Q5/out/production/Q5/com/company/Main.class
    	new file:   Problem-set 4/Q5/src/com/company/Main.java

--- Problem-set 4/Q4/src/com/company/Main.java
+++ Problem-set 4/Q4/src/com/company/Main.java
@@ -3,19 +3,19 @@ package com.company;
 public class Main {
 
     public static int yearsTilOneMillion(double initialBalance) {
-        int counter = 0;
+        int counter = 1;
         if (initialBalance > 1000000) {
             return 0;
         } else {
             while (initialBalance<1000000){
-                initialBalance*=1.05;
                 counter++;
+                initialBalance*=1.05;
             }
             return counter;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(yearsTilOneMillion(1));
+        System.out.println(yearsTilOneMillion(1000));
     }
 }
