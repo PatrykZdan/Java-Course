7f12fb8b800771f6dfbadc2327e6debd0bc7aff9
Patryk Zdanowicz <patrykzdan@gmail.com>
2019-05-29 16:25:48 +0200

    Done question 6 from Problem-Set 3.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   Problem-set 3/Q5/.idea/workspace.xml
    	new file:   Problem-set 3/Q6/.idea/description.html
    	new file:   Problem-set 3/Q6/.idea/encodings.xml
    	new file:   Problem-set 3/Q6/.idea/misc.xml
    	new file:   Problem-set 3/Q6/.idea/modules.xml
    	new file:   Problem-set 3/Q6/.idea/project-template.xml
    	new file:   Problem-set 3/Q6/.idea/vcs.xml
    	new file:   Problem-set 3/Q6/.idea/workspace.xml
    	new file:   Problem-set 3/Q6/Q6.iml
    	new file:   Problem-set 3/Q6/out/production/Q6/com/company/Main.class
    	new file:   Problem-set 3/Q6/src/com/company/Main.java

---
+++ Problem-set 3/Q6/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static String nametagText(String name){
+        String returnText="Hello, my name is "+name;
+        return  returnText;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(nametagText("Patryk"));
+    }
+}
