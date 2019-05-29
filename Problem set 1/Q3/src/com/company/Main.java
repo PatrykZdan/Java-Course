e13e20a2481de39667d1161ce209edb760c988ef
Patryk Zdanowicz <patrykzdan@gmail.com>
2019-05-29 14:56:48 +0200

    Reorganization of folders and files. Done Q3 from Problem-Set-1
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   .idea/workspace.xml
    	renamed:    .idea/description.html -> HelloWorld/.idea/description.html
    	renamed:    .idea/encodings.xml -> HelloWorld/.idea/encodings.xml
    	renamed:    .idea/misc.xml -> HelloWorld/.idea/misc.xml
    	renamed:    .idea/modules.xml -> HelloWorld/.idea/modules.xml
    	renamed:    .idea/project-template.xml -> HelloWorld/.idea/project-template.xml
    	renamed:    .idea/vcs.xml -> HelloWorld/.idea/vcs.xml
    	new file:   HelloWorld/.idea/workspace.xml
    	renamed:    Udacity Java Course.iml -> HelloWorld/Udacity Java Course.iml
    	renamed:    out/production/Udacity Java Course/com/company/Main.class -> HelloWorld/out/production/Udacity Java Course/com/company/Main.class
    	renamed:    src/com/company/Main.java -> HelloWorld/src/com/company/Main.java
    	new file:   Problem set 1/Q3/.idea/description.html
    	new file:   Problem set 1/Q3/.idea/encodings.xml
    	new file:   Problem set 1/Q3/.idea/misc.xml
    	new file:   Problem set 1/Q3/.idea/modules.xml
    	new file:   Problem set 1/Q3/.idea/project-template.xml
    	new file:   Problem set 1/Q3/.idea/vcs.xml
    	new file:   Problem set 1/Q3/.idea/workspace.xml
    	new file:   Problem set 1/Q3/Q3.iml
    	new file:   Problem set 1/Q3/out/production/Q3/com/company/Main.class
    	new file:   Problem set 1/Q3/src/com/company/Main.java

---
+++ Problem set 1/Q3/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int bankBalance = 500;
+        bankBalance += 250;
+        bankBalance = bankBalance/100;
+        System.out.println(bankBalance);
+    }
+}
