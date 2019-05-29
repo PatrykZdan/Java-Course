36c92b44275cfd9d844968b9bb960af3b7af8f2e
Patryk Zdanowicz <patrykzdan@gmail.com>
2019-05-29 15:05:14 +0200

    Done question 6 from Problem-set-1
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   Problem set 1/Q5/.idea/workspace.xml
    	new file:   Problem set 1/Q6/.idea/misc.xml
    	new file:   Problem set 1/Q6/.idea/modules.xml
    	new file:   Problem set 1/Q6/.idea/vcs.xml
    	new file:   Problem set 1/Q6/.idea/workspace.xml
    	new file:   Problem set 1/Q6/Q6.iml
    	new file:   Problem set 1/Q6/out/production/Q6/Main.class
    	new file:   Problem set 1/Q6/src/Main.java

---
+++ Problem set 1/Q6/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String firstName = "Patryk",
+                lastName = "Zdanowicz",
+                fullName = firstName + " " + lastName;
+
+        System.out.println("Hello, my name is "+fullName);
+        System.out.println("There are "+firstName.length()+" letters in my name");
+
+    }
+}
