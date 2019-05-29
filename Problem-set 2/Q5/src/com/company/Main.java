def310d571fdb42c9e7045d9a5f137fbd46b77ff
Patryk Zdanowicz <patrykzdan@gmail.com>
2019-05-29 15:35:15 +0200

    Done question 5 from Problem-Set 2.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   Problem-set 2/Q4/.idea/workspace.xml
    	new file:   Problem-set 2/Q5/.idea/description.html
    	new file:   Problem-set 2/Q5/.idea/encodings.xml
    	new file:   Problem-set 2/Q5/.idea/misc.xml
    	new file:   Problem-set 2/Q5/.idea/modules.xml
    	new file:   Problem-set 2/Q5/.idea/project-template.xml
    	new file:   Problem-set 2/Q5/.idea/vcs.xml
    	new file:   Problem-set 2/Q5/.idea/workspace.xml
    	new file:   Problem-set 2/Q5/Q5.iml
    	new file:   Problem-set 2/Q5/src/com/company/Main.java

---
+++ Problem-set 2/Q5/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int time = 18;
+        String timeOfDay;
+
+        if(time>=5&&time<12){
+            timeOfDay = "morning";
+        } else if(time>=12&&time<20){
+            timeOfDay = "daytime";
+        }  else {
+            timeOfDay = "night";
+        }
+
+    }
+}
