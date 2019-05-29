576dc78ae1227d10b6c1befde6c1dc8eb64dd26f
Patryk Zdanowicz <patrykzdan@gmail.com>
2019-05-29 16:15:17 +0200

    Done question 4 from Problem-Set 3.
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   Problem-set 3/Q2/.idea/workspace.xml
    	new file:   Problem-set 3/Q4/.idea/description.html
    	new file:   Problem-set 3/Q4/.idea/encodings.xml
    	new file:   Problem-set 3/Q4/.idea/misc.xml
    	new file:   Problem-set 3/Q4/.idea/modules.xml
    	new file:   Problem-set 3/Q4/.idea/project-template.xml
    	new file:   Problem-set 3/Q4/.idea/vcs.xml
    	new file:   Problem-set 3/Q4/.idea/workspace.xml
    	new file:   Problem-set 3/Q4/Q4.iml
    	new file:   Problem-set 3/Q4/src/com/company/Main.java

---
+++ Problem-set 3/Q4/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        public double absoluteValue(double x) {
+            if (x < 0) {
+                return -x;
+            } else {
+                return x;
+            }
+        }
+    }
+}
